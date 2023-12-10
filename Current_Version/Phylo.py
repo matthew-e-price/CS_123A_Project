eb6f298ab741163673bd10d0e235d3b946f52817
price <priceypersonal@hotmail.com>
2023-12-09 18:59:14 -0800

    Added Ru's changes to what we had, and changed up some directory stuff.

--- Current_Version/Phylo.py
+++ Current_Version/Phylo.py
@@ -4,10 +4,18 @@ from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstruct
 from Bio import Phylo
 import io
 
+from matplotlib import pyplot as plt
+
+# Set this to the absolute path of your clustalo.exe
+CLUSTAL_PATH = "C:/Users/price/PycharmProjects/CS_123A_Project/Clustal_Windows/clustalo.exe"
+
+
 # Perform MSA using Clustal Omega
 def perform_msa(input_file, output_file):
-    clustalomega_cline = ClustalOmegaCommandline(infile=input_file, outfile=output_file, verbose=True, auto=True, cmd="/Users/ruchadeo/Downloads/clustalo")
+    clustalomega_cline = ClustalOmegaCommandline(infile=input_file, outfile=output_file, verbose=True, auto=True,
+                                                 cmd=CLUSTAL_PATH)
     stdout, stderr = clustalomega_cline()
+
 
 # Calculate Distance Matrix from Aligned Sequences
 def calculate_distance_matrix(aligned_sequences_file):
@@ -15,6 +23,7 @@ def calculate_distance_matrix(aligned_sequences_file):
     calculator = DistanceCalculator('identity')
     distance_matrix = calculator.get_distance(alignment)
     return distance_matrix
+
 
 # Construct Phylogenetic Tree
 def construct_tree(distance_matrix, algorithm='upgma'):
@@ -27,15 +36,17 @@ def construct_tree(distance_matrix, algorithm='upgma'):
         raise ValueError("Unsupported algorithm. Please choose either 'upgma' or 'nj'.")
     return tree
 
+
 # Output tree in Newick Format
 def tree_to_newick(tree):
     newick_io = io.StringIO()
     Phylo.write(tree, newick_io, 'newick')
     return newick_io.getvalue().strip()
 
+
 # Replace 'input_sequences.fasta' with your actual input file containing the sequences
-input_sequences_file = '/Users/ruchadeo/Downloads/sampleseq.fasta.txt'
-aligned_sequences_file = '/Users/ruchadeo/Downloads/outseq7.txt'
+input_sequences_file = 'sampleseq.txt'
+aligned_sequences_file = 'outseq7.txt'
 
 # Step 1: Perform MSA
 perform_msa(input_sequences_file, aligned_sequences_file)
@@ -59,3 +70,17 @@ print("UPGMA Tree in Newick Format:")
 print(upgma_newick)
 print("\nNJ Tree in Newick Format:")
 print(nj_newick)
+
+print("UPGMA Tree:")
+Phylo.draw_ascii(upgma_tree)
+
+print("\nNJ Tree:")
+Phylo.draw_ascii(nj_tree)
+
+fig1 = plt.figure(figsize=(13, 5), dpi=100)
+axes1 = fig1.add_subplot(1, 1, 1)
+Phylo.draw(upgma_tree, axes=axes1)
+
+fig2 = plt.figure(figsize=(13, 5), dpi=100)
+axes2 = fig2.add_subplot(1, 1, 1)
+Phylo.draw(nj_tree, axes=axes2)
